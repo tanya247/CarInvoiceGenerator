87c71c3160dde8f7ed6b52207cc3541a613f580b
user name <user email>
2021-05-13 11:26:54 +0530

    UC1 showed

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,8 +1,15 @@
 public class InvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1  ;
+    private static final double MINIMUMFARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return ((distance * MINIMUM_COST_PER_KILOMETER) + (time * COST_PER_TIME));
+        double fare =  ((distance * MINIMUM_COST_PER_KILOMETER) + (time * COST_PER_TIME));
+        if(fare < MINIMUMFARE){
+            return MINIMUMFARE;
+        }
+        else{
+            return fare;
+        }
     }
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -3,12 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
     @Test
-    void givenDistanceAndTimeShouldReturnTotalFare() {
+    public void theGivenDistanceAndTimeShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance,time);
         System.out.println("Total Fare is : Rs. "+ fare);
         Assertions.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        System.out.println("Total Fare is : Rs. "+ fare);
+        Assertions.assertEquals(5,fare,0.0);
     }
 }
